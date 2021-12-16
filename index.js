11e79df8fbb3c26507fd0d3544f02b9243e73e78
IT Hit <build@ithit.com>
2021-12-16 08:52:22 +0000

    v5.21.5815

---
+++ index.js
@@ -0,0 +1,8 @@
+// -----------------------------------------------------------------------
+// IT Hit WebDAV Ajax Library v5.21.5815.0
+// Copyright © 2020 IT Hit LTD. All rights reserved.
+// License: https://www.webdavsystem.com/ajax/
+// -----------------------------------------------------------------------
+
+import "./ITHitWebDAVClient";
+export const ITHit = window.ITHit
